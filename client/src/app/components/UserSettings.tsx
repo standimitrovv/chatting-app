8a69a24338e645d4f611b9610970ee6300e44ec4
Stanimir Dimitrov <evlution63@gmail.com>
2022-07-07 10:42:01 +0300

    fix: position of statusIcon in settings

--- client/src/app/components/UserSettings.tsx
+++ client/src/app/components/UserSettings.tsx
@@ -43,7 +43,10 @@ export const UserSettings: React.FC<Props> = ({
               className='rounded-full w-20 h-20'
               referrerPolicy='no-referrer'
             />
-            <StatusIcon status={!userStatus ? 'Offline' : userStatus} />
+            <StatusIcon
+              className='absolute top-14 -right-1'
+              status={!userStatus ? 'Offline' : userStatus}
+            />
           </div>
           <div className='mr-auto flex flex-col'>
             <p className='ml-4 font-semibold'>{userCredentials?.fullName}</p>
