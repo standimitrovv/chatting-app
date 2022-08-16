faae4ff8818584d8fec38e00b766d43366a6de67
Stanimir Dimitrov <evlution63@gmail.com>
2022-08-16 09:39:30 +0300

    wip

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const app = express();
+import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 //routes
 const chatRoutes = require('./routes/chat');
 const userRoutes = require('./routes/user');
 const conversationRoutes = require('./routes/conversation');
 const messageRoutes = require('./routes/message');
+
+dotenv.config();
+
+const app = express();
 
 // API essentials
 app.use(express.json());
@@ -20,11 +23,11 @@ app.use('/users', userRoutes);
 app.use('/conversations', conversationRoutes);
 app.use('/messages', messageRoutes);
 
-app.use((req, res, next) => {
+app.use(() => {
   throw new Error('Not implemented!');
 });
 
-app.use((error, req, res, next) => {
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   // BUG - error handling is not working - message.js - getConvoMessages
   if (res.headersSent) {
     return next(error);
@@ -36,8 +39,8 @@ app.use((error, req, res, next) => {
   });
 });
 
-mongoose.connect(process.env.MONGODB_CONNECT).then((result) => {
+mongoose.connect(process.env.MONGODB_CONNECT!).then((result) => {
   const server = app.listen(3001);
   const io = require('./socket').init(server);
-  io.on('connection', (socket) => console.log('Client connected'));
+  io.on('connection', () => console.log('Client connected'));
 });
