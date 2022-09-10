4a35173fff97e9f077b389356c75fe41a94f9a3f
Stanimir Dimitrov <evlution63@gmail.com>
2022-09-10 16:41:12 +0300

    refactor: extend the User model for getUsers service util

--- packages/client/src/service/user/GetUsers.ts
+++ packages/client/src/service/user/GetUsers.ts
@@ -2,6 +2,8 @@ import { User } from '@chapp/server/models/user';
 import axios from 'axios';
 import { API } from '../Api';
 
+interface GetUsersResult extends User {}
+
 export const getUsers = async () => {
-  return await axios.get<string, User[]>(`${API}/users/get-users`);
+  return await axios.get<string, GetUsersResult[]>(`${API}/users/get-users`);
 };
