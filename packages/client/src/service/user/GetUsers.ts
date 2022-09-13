4dbdcb40aca115dc99edcb5741fba7078d20285a
Stanimir Dimitrov <evlution63@gmail.com>
2022-09-13 09:57:30 +0300

    refactor: remove await after return

--- packages/client/src/service/user/GetUsers.ts
+++ packages/client/src/service/user/GetUsers.ts
@@ -5,7 +5,7 @@ import { API } from '../Api';
 interface GetUsersResult extends User {}
 
 export const getUsers = async () => {
-  return await axios.get<string, GetUsersResult[]>(`${API}/users/get-users`);
+  return axios.get<string, GetUsersResult[]>(`${API}/users/get-users`);
 };
 
 export const getAllUsersExceptCurrentOne = async (userId: string) => {
